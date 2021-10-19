b3a1206268cd604bf8b5367bacd9ededc6947946
mepraveenofficial@gmail.com <mepraveenofficial@gmail.com>
2021-10-19 12:30:34 +0530

    Sync main

--- controller/search.js
+++ controller/search.js
@@ -3,24 +3,30 @@ const { config } = require("dotenv");
 config();
 
 const search = async (req, res, next) => {
-  const imageBaseUrl = process.env.URL_staticfiles;
+  if (req.body.search === undefined) {
+    res.send("no match found");
+  } else {
+    console.log(req.body.search);
+    const imageBaseUrl = process.env.URL_staticfiles;
+    //   { $regex: req.body.search, $options: "i" }
+    try {
+      const data = await product
+        .find({ productName: { $regex: req.body.search, $options: "i" } })
+        .lean();
+      if (data[0]) {
+        for (let i in data) {
+          let imageName = data[i].image;
+          data[i].image = imageBaseUrl + imageName;
+        }
 
-  try {
-    const data = await product
-      .find({ productName: { $regex: req.body.search, $options: "i" } })
-      .lean();
-    if (data[0]) {
-      for (let i in data) {
-        let imageName = data[i].image;
-        data[i].image = imageBaseUrl + imageName;
+        res.status(200).send(data);
+      } else {
+        res.send("no match found");
       }
-      res.status(200).send(data);
-    } else {
-      res.send("no match found");
+      console.log(data);
+    } catch (error) {
+      res.send(error);
     }
-    console.log(data);
-  } catch (error) {
-    res.send(error);
   }
 };
 
